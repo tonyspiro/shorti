3c54fb2ad4121829a9fada0ed65b72082a6f81d3
Tony Spiro <tspiro@tonyspiro.com>
2015-11-23 21:06:09 -0600

    react ex

--- examples/react/app.js
+++ examples/react/app.js
@@ -1,13 +1,13 @@
 // app.js
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Shorti from 'shorti';
 
 class App extends Component {
 
 	render(){
 
-		let style = Shorti('w-500 h-210 p-40 font-50 relative t-100 l-50p ml-n205 solid bw-1 bc-444');
+		let style = Shorti('w-500 h-210 p-40 font-50 relative t-100 l-50 solid bw-1 bc-ccc');
 
 		return <div style={ style }>
 			Hello world
@@ -16,7 +16,7 @@ class App extends Component {
 
 };
 
-ReactDOM.render(
+render(
 	<App />,
 	document.getElementById('app-root')
 );
