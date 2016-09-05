890247a68bfa6b7dbe525d58affedb4a82065bf6
Emil Sedgh <emilsedgh@kde.org>
2016-09-05 16:12:53 +0200

    Adding contain to index as well

--- index.js
+++ index.js
@@ -205,6 +205,10 @@ module.exports = function(class_string){
                 var property = 'backgroundSize';
                 var value = prop_element;
               }
+              if (prop_element == 'contain') {
+                var property = 'backgroundSize';
+                var value = prop_element;
+              }
               if (prop_element == 'center') {
                 var property = 'backgroundPosition';
                 var value = prop_element;
